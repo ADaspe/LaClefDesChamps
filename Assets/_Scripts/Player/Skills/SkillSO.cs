95b312e1db8e6767678fb675172e3224b2f1d494
Alexandre Daspe <alexandre.daspe@gmail.com>
2021-08-02 14:31:44 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/_Scripts/Player/Skills/SkillSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Skill", menuName = "Scriptable Objects/Skill", order = 2)]
+public class SkillSO : ScriptableObject
+{
+
+
+}
