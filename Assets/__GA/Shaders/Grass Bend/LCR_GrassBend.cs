d0a90b4b81ed7222f11bec20bf0ced9b3b710f27
Lucrezia <lucrezia.romeo@gmail.com>
2021-08-05 13:47:21 +0200

    bendgrass

---
+++ Assets/__GA/Shaders/Grass Bend/LCR_GrassBend.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LCR_GrassBend : MonoBehaviour
+{
+    void Update()
+    {
+        Vector4 char_pos = gameObject.transform.position;
+        Shader.SetGlobalVector("kqnx_char_pos", char_pos);
+    }
+}
